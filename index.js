45679399d7daf38bd0d437dda692db9a903fe78d
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-16 20:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ return string.toLowerCase()
 function logShout(string) {
   console.log (string.toUpperCase())
 }
-function logWhispter(string) {
+function logWhisper(string) {
   console.log (string.toLowerCase())
 }
